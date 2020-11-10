9e95ea3531ecd27d8d1ee4134e1f501177af35ca
Melanie Hall <hallmt313@gmail.com>
2020-11-09 19:29:19 -0500

    et.js general inquirer structure

--- et.js
+++ et.js
@@ -17,6 +17,103 @@ connection.connect(function(err) {
 });
 
 function start(){
+  inquirer.prompt({
+    name: "startPrompt",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "View All Employees",
+      "View All Employees by Department",
+      "View All Employees by Manager",
+      "Add Employee",
+      "Remove Employee",
+      "Update Employee Role",
+      "Update Employee Manager",
+      "View All Roles",
+      "Add a Role",
+      "Remove a Role",
+      "I'm done."
+    ]
+  })
+  .then(function(answer){
+    switch(answer.startPrompt) {
+      case choices[0]:
+        viewAllEmpl();
+        break;
+      case choices[1]:
+        viewAllbyDept();
+        break;
+      case choices[2]:
+        viewAllbyMang();
+        break;
+      case choices[3]:
+        addEmpl();
+        break;
+      case choices[4]:
+        removeEmpl();
+        break;
+      case choices[5]:
+        updateEmplRole();
+        break;
+      case choices[6]:
+        updateEmplMang();
+        break;
+      case choices[7]:
+        viewAllRoles();
+        break;
+      case choices[8]:
+        addRole();
+        break;
+      case choices[9]:
+        removeRole();
+        break;
+      case choices[10]:
+        console.log("Thank you for using Employee Tracker.");
+        return
+      default:
+        console.log("Please select an option.");
+        start();
+    }
+  });
+}
 
+function viewAllEmpl(){
+
+}
+
+function viewAllbyDept(){
+
+}
+
+function viewAllbyMang (){
+
+}
+
+function addEmpl(){
+
+}
+
+function removeEmpl(){
+
+}
+
+function updateEmplRole(){
+
+}
+
+function updateEmplRole(){
+
+}
+
+function viewAllRoles(){
+
+}
+
+function addRole(){
+
+}
+
+function removeRole(){
+  
 }
 
